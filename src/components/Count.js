8f3ca5649f499d145531255d914bb92d7efee757
Todd Albert <todd@bocacode.com>
2023-02-02 12:00:41 -0500

    wrote function for all buttons

--- src/components/Count.js
+++ src/components/Count.js
@@ -2,13 +2,20 @@ import React, { useState } from 'react';
 
 export default function Count() {
   const [count, setCount] = useState(0)
+  const addCount = (change) => {
+    let newCount = count
+    newCount = newCount + change
+    setCount(newCount)
+  }
   return (
     <>
       <h1>You clicked the button {count} times.</h1>
       <div>
-        <button onClick={() => setCount(count - 1)}>-1</button>
-        <button onClick={() => setCount(0)}>Reset</button>
-        <button onClick={() => setCount(count + 1)}>+1</button>
+        <button onClick={() => addCount(-10)}>-10</button>
+        <button onClick={() => addCount(-1)}>-1</button>
+        <button onClick={() => addCount(-count)}>Reset</button>
+        <button onClick={() => addCount(1)}>+1</button>
+        <button onClick={() => addCount(10)}>+10</button>
       </div>
     </>
   )
